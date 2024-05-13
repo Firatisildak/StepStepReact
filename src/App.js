a018e46a70db811ae44fd78ca0f5370050b28712
Fırat Işıldak <firatisildak1@gmail.com>
2024-05-13 13:19:20 +0300

    data transfer between components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
 import CategoryList from './CategoryList';
+import React, { Component } from 'react';
 import Navi from './Navi';
 import ProductList from './ProductList';
 import { Container, Row, Col } from "reactstrap";
 
-function App() {
-  let productInfo={title:"ProductList"}
-  let categoryInfo={title:"CategoryList"}
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
-        <Row>
-          <Col xs="3">
-            <CategoryList info={categoryInfo} />
-          </Col>
-          <Col xs="9">
-            <ProductList info={productInfo} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
-}
+export default class App extends Component {
+  state={currentCategory:""}
+  changeCategory = (category) => { this.setState({ currentCategory: category.categoryName }) };
 
-export default App;
+  render() {
+    let productInfo = { title: "ProductList" }
+    let categoryInfo = { title: "CategoryList" }
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
+            </Col>
+            <Col xs="9">
+              <ProductList currentCategory={this.state.currentCategory} info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
