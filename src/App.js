d0d1ed119e1cacc06885e1c845b805f834cb1131
Fırat Işıldak <firatisildak1@gmail.com>
2024-05-14 14:21:24 +0300

    fetch from api

--- src/App.js
+++ src/App.js
@@ -5,9 +5,20 @@ import ProductList from './ProductList';
 import { Container, Row, Col } from "reactstrap";
 
 export default class App extends Component {
-  state={currentCategory:""}
-  changeCategory = (category) => { this.setState({ currentCategory: category.categoryName }) };
-
+  state={currentCategory:"",products:[]};
+  componentDidMount(){
+    this.getProducts();
+  }
+  changeCategory = (category) => { this.setState({ currentCategory: category.categoryName }) 
+  this.getProducts(category.id);
+};
+  getProducts=categoryId=>{
+    let url="http://localhost:3000/products";
+    if(categoryId){
+      url+="?categoryId="+categoryId;
+    }
+    fetch(url).then(response=>response.json()).then(data=>this.setState({products:data}));
+  }
   render() {
     let productInfo = { title: "ProductList" }
     let categoryInfo = { title: "CategoryList" }
@@ -22,7 +33,7 @@ export default class App extends Component {
               <CategoryList currentCategory={this.state.currentCategory} changeCategory={this.changeCategory} info={categoryInfo} />
             </Col>
             <Col xs="9">
-              <ProductList currentCategory={this.state.currentCategory} info={productInfo} />
+              <ProductList products={this.state.products} currentCategory={this.state.currentCategory} info={productInfo} />
             </Col>
           </Row>
         </Container>
